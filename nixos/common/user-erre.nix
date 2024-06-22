e7d144201f84d331cbc2895ffd0b83790b4c80cd
Eric Thorburn <tabulateyard962@gmail.com>
2024-06-22 21:10:47 +0200

    change to actual name

--- nixos/common/user-erre.nix
+++ nixos/common/user-erre.nix
@@ -2,7 +2,7 @@
 {
   users.users."erre" = {
     isNormalUser = true;
-    initialPasswordHash = "$y$j9T$I6MeqBGtpLepoMdQO3ccw0$zaA5hlZYCX/NQZlgURIa.ucaq4JKVrMcAa8jvUPSAo8";
+    initialHashedPassword = "$y$j9T$I6MeqBGtpLepoMdQO3ccw0$zaA5hlZYCX/NQZlgURIa.ucaq4JKVrMcAa8jvUPSAo8";
     extraGroups = ["wheel" "ssh-access"];
     packages = with pkgs; [helix];
     openssh.authorizedKeys.keys = [];
