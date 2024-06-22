364982979e60a97e9cbd70efd80022fe5cf152aa
Eric Thorburn <tabulateyard962@gmail.com>
2024-06-22 21:59:36 +0200

    add file to systemdify system

---
+++ nixos/systemdify.nix
@@ -0,0 +1,15 @@
+#
+# systemd options, to make system more coherent
+# if i am using systemd, i might aswell use the whole thing :D
+#
+{lib, ...}:
+{
+  # Use systemd for the initial ramdisk
+  boot.initrd.systemd.enable = lib.mkDefault true;
+
+  # Use systemd-networkd for networking (translate script-based networking)
+  networking.useNetworkd = lib.mkDefault true;
+
+  # Use resolved instead of resolvconf
+  services.resolved.enable = lib.mkDefault true;
+}
